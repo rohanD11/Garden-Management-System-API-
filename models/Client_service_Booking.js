0eab8688aa342923833c1ad1f6ac6d6d86670adb
rohanD11 <rohandupade11@gmail.com>
2023-10-10 19:19:50 +0530

    added time and date

--- models/Client_service_Booking.js
+++ models/Client_service_Booking.js
@@ -34,6 +34,12 @@ const ServiceSchema = mongoose.Schema({
   amount_payable: {
     type: String,
   },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("clientService", ServiceSchema);
